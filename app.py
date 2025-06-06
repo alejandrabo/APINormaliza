1c8b5d523d29a79c6e7f383696ff7894f94e4949
alejandrabo <alejandra.buriticad@gmail.com>
2025-06-06 13:27:06 -0500

    nueva conexion app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from postal.parser import parse_address
 import re
 import unicodedata
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -142,4 +143,8 @@ def normalizar():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=5000)
